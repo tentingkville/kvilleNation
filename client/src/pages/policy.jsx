4b2b90a33a2e48fadfae320621a2dc80f89ef6fe
Kelvin Bueno <kelvinbueno@Kelvins-MacBook-Pro.local>
2024-03-20 13:18:27 -0400

    added policy

--- client/src/pages/policy.jsx
+++ client/src/pages/policy.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import policyPdf from '../components/policy.pdf'; // Import the PDF file
+import '../styles/policy.css'; // Import the CSS file
 
 function Policy() {
+    const [pdfLoaded, setPdfLoaded] = useState(false);
+
+    useEffect(() => {
+        const preloadPdf = new Image();
+        preloadPdf.src = policyPdf;
+        preloadPdf.onload = () => setPdfLoaded(true);
+    }, []);
+
     return (
-        <div>
-            <h1>Policy</h1>
+        <div className="policy-container">
+            {pdfLoaded ? (
+                <div className="policy-bubble">
+                    <object
+                        type="application/pdf"
+                        data={policyPdf}
+                        width="100%"
+                        height="100%"
+                    >
+                        <p>Your browser does not support PDFs.
+                            <a href={policyPdf}>Download the PDF</a>.
+                        </p>
+                    </object>
+                </div>
+            ) : (
+                <p>Loading PDF...</p>
+            )}
         </div>
     );
 }
